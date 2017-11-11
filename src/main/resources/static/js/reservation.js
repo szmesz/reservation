9f09d11da23e7adb342bc884381539e6a2912977
szmesz <vajda.tamas.kk.1@gmail.com>
2017-11-11 22:12:55 +0000

    Add some more exceptions and designs

--- src/main/resources/static/js/reservation.js
+++ src/main/resources/static/js/reservation.js
@@ -18,11 +18,13 @@
   function alertMessageAccordingToStatusCode(status) {
 	  
 	  switch (status) { 
-	  	case 400: 
-			alert("Reservation failed - please check your input for completeness.");
-			break;
+	    case 400:
+	    	alert("Reservation failed - please check if you input valid values into the input fields " +
+	    		  "such as only numbers greater than 0 into the Facility Id field " +
+	    		  "and correct form of date to the date fields such as dd/mm/yyyy or yyyy-mm-dd " +
+	    		  "and the Start Date preceeds the End Date " +
+	    		  "Please make sure you filled all the fields")
 		case 405: 
-			alert("Reservation failed - please check your input for completeness.");
 			break;
 		case 409: 
 			alert('The room is already reserved for this period, please choose a different period.');
@@ -30,11 +32,14 @@
 		case 500: 
 			alert('Reservation failed - please try again later.');
 			break;		
-		default:
-			alert('Reservation failed - please try again later.');
+		
 	}
   }
- 
+  	  function alertSuccess(reservationId) {
+  		alert("Your reservation has been successfully processed. Your reservation id is: " + reservationId);
+  		
+	}
+  	 
   
 $(document).ready(function() {
 
@@ -45,13 +50,33 @@ $(document).ready(function() {
     
     var reservationRequest = collectFormInput();
     
-    // TODO: form input validation
+    var resourceId=(reservationRequest.resourceId);
+    if (reservationRequest.resourceId<1) { 
+		alert("Reservation failed - Facility Id: must be a number greater than 0");
+    }
+    
+    var fromDateInMillis=Date.parse(reservationRequest.fromDate);
+    var toDateInMillis=Date.parse(reservationRequest.toDate);
+    if (isNaN(fromDateInMillis)==false && isNaN(toDateInMillis) == false)
+    { 
+    	if (fromDateInMillis > toDateInMillis) {
+    		alert("Please make sure the Start Date preceeds the End Date");
+    	}
+    }   
+    
+   /* var owner = reservationRequest.owner;
+    var regexp = /\d"+/;
+    if (regexp.exec(reservationRequest.owner) !== 0) 
+    {
+       alert("Reservation failed - The 'Reserved by' field has to be filled with letters");
+    } */
     
     var reservationUrl = 
       '/api/reservations/' + encodeURIComponent(reservationRequest.resourceId) 
       + '/from-' + encodeURIComponent(reservationRequest.fromDate) 
       + '/to-' + encodeURIComponent(reservationRequest.toDate)
       + '/?owner=' + encodeURIComponent(reservationRequest.owner);
+  
 
     $.ajax({
       url : reservationUrl,                    
@@ -60,19 +85,21 @@ $(document).ready(function() {
       cache : false,                         
       timeout : 5000,                     
 
-      data : {},                               
-
-      success : function(data, statusText, response) {          
-        var reservationId = response.getResponseHeader('reservation-id');
+      data : {},   
+      
+      success : function(data, statusText, response) {
+    	var resourceId = response.getResponseHeader('resource-id');  
         $('#reservationId').text(reservationId);
-        $('#reservation-successful-message').show();
+       /* $('#reservation-successful-message').show();*/
+     	alertSuccess(reservationId); 
+     
       },
                                                 
       error : function(XMLHttpRequest, textStatus, errorThrown) {   
         console.log("reservation request failed ... HTTP status code: " + XMLHttpRequest.status + ' message ' + XMLHttpRequest.responseText);
         alertMessageAccordingToStatusCode(XMLHttpRequest.status);
-       /* var errorCodeToHtmlIdMap = {400 : '#validation-error', 405 : '#validation-error', 409 : '#conflict-error' , 500: '#system-error'};
-        var id = errorCodeToHtmlIdMap[XMLHttpRequest.status];
+        var errorCodeToHtmlIdMap = {400 : '#validation-error', 405 : '#validation-error', 409 : '#conflict-error' , 500: '#system-error'};
+       /* var id = errorCodeToHtmlIdMap[XMLHttpRequest.status];
         if (!id) {
           id =  errorCodeToHtmlIdMap[500]; 
         }
